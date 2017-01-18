b4aa9401ba3924bbf0738490747abcc729f24091
fang53215 <fangyan53215@sina.com>
2017-01-18 10:33:51 +0800

    interactive

--- angular/js/controller/interactive.controller.js
+++ angular/js/controller/interactive.controller.js
@@ -10,3 +10,38 @@ interactiveModule.controller("interactiveCtrl", function($scope, $http, $state, 
 		$scope.datas = data;
 	});
 })
+
+/*互动专区--更多*/
+.controller("interactiveMoreCtrl", function($scope, $http, $state, $stateParams){
+	
+	$scope.toggleList = {current:0};
+	$scope.setCurrent = function(param){
+        $scope.toggleList.current = param;
+	}
+
+	$http.get("../angular/data/interactiveLeftMenu.json")
+	.success(function(data){   
+		$scope.data1 = data;
+	});
+
+	$scope.titleName = "增值税";
+	$scope.getData = function(param){
+		$scope.titleName = param;
+	}
+
+	$http.get("../angular/data/interactiveMore.json")
+	.success(function(data){   
+		$scope.data2 = data;
+	});
+})
+/*互动专区--新的问题*/
+.controller("interactiveQuestionCtrl", function($scope, $http, $state, $stateParams){
+	$http.get("../angular/data/interactiveMore.json")
+	.success(function(data){   
+		$scope.data1 = data;
+	});
+	$http.get("../angular/data/interactiveQuestion.json")
+	.success(function(data){   
+		$scope.data2 = data;
+	});
+})
